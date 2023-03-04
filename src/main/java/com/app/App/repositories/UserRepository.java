0fe0ba984fadb8c120eff2e5487dd62540f4c3d4
IJuzenas <juzenasignas@gmail.com>
2023-03-04 18:32:25 +0200

    Added query in user repository

--- src/main/java/com/app/App/repositories/UserRepository.java
+++ src/main/java/com/app/App/repositories/UserRepository.java
@@ -2,11 +2,14 @@ package com.app.App.repositories;
 
 import com.app.App.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT ^ FROM USER", nativeQuery = true)
     List<User> getAllUsers();
 }
